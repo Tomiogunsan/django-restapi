d108cec049f8b30cfa622a2e83e6d3d503ee70ed
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-04-27 21:03:04 +0100

    ch: implemented serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,15 +3,16 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from product.models import Product
 from django.forms.models import model_to_dict
+from product.serializers import ProductSerializer
 import json
 
 @api_view(['GET', 'POST'])
 def api_home(request, *args, **kwargs):
     if request.method != 'POST':
         return Response({'detail': 'Method not allowed'}, status=405)
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields= ['id', 'title', 'price', 'sale_price'])
-        
+    if instance:
+        # data = model_to_dict(model_data, fields= ['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
---
+++ backend/product/serializers.py
@@ -0,0 +1,16 @@
+from django import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'content',
+            'price',
+            'sale_price',
+            'my_discount',
+        ]
+    def get_my_discount(self, obj):
+        return obj.get_discount()
