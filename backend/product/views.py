e8923b09bf06cef09e15e6eaaea9b076e7c5bd10
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-04-28 16:29:59 +0100

    ch: implemented generics

--- backend/product/views.py
+++ backend/product/views.py
@@ -3,6 +3,7 @@ from .serializers import ProductSerializer
 from rest_framework import generics, mixins
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -30,7 +31,9 @@ def product_alt_view(request,pk=None, *args, **kwargs):
     
     if method == 'GET':
         if pk is not None:
-            return Response()
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
         
         queryset = Product.objects.all()
         data = ProductSerializer(queryset, many=True).data
@@ -38,11 +41,11 @@ def product_alt_view(request,pk=None, *args, **kwargs):
     if method == 'POST':
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            # title = serializer.validated_data.get('title')
-            # content = serializer.validated_data.get('content') or None
-            # if content is None:
-            #     content = title
-            # serializer.save(content=content)
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
             return Response(serializer.data)
         return Response({"invalid": "not good data"}, status=400)
         
