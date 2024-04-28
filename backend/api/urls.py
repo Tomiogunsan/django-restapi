9afe35d93dc299d84035882ec523b3af33581940
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-04-28 15:37:07 +0100

    ch: implemented generics

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.api_home)
+    path('', views.api_home),
+    # path('api/products/', include('product.urls'))
 ]
